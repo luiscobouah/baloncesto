75dba1a28ff55fe5f95a30fc0e87c04d1f513c70
Luis Alberto Cobo <luisalberto.cobo@vass.es>
2020-12-23 19:23:13 +0100

    PU actualizarJugador

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -1,70 +1,31 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.dbunit.IDatabaseTester;
-import org.dbunit.JdbcDatabaseTester;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
+import static org.assertj.db.api.Assertions.assertThat;
+import org.assertj.db.type.Source;
+import org.assertj.db.type.Table;
 
 public class ModeloDatosTest {
+    String dbHost = "jdbc:mysql://localhost";
+    String dbPort = "3309";
+    String dbName = "baloncesto";
+    String dbUser = "usuario";
+    String dbPass = "clave";
+    String url = dbHost + ":" + dbPort + "/" + dbName;
 
-    public final static String FILENAME = "/dbunitData.xml";
-
-    @BeforeEach
-    public void loadData() throws Exception {
-        String dbHost = System.getenv().get("DATABASE_HOST");
-        String dbPort = System.getenv().get("DATABASE_PORT");
-        String dbName = System.getenv().get("DATABASE_NAME");
-        String dbUser = System.getenv().get("DATABASE_USER");
-        String dbPass = System.getenv().get("DATABASE_PASS");
-        String url = dbHost + ":" + dbPort + "/" + dbName;
-
-        IDatabaseTester databaseTester = new JdbcDatabaseTester(
-                "com.mysql.cj.jdbc.Driver",
-                url,
-                dbUser,
-                dbPass,
-                dbName
-        );
-
-        IDataSet dataSet = new FlatXmlDataSetBuilder().build(this.getClass()
-                .getResourceAsStream(FILENAME));
-        databaseTester.setDataSet(dataSet);
-        databaseTester.onSetup();
-    }
-
-
-
-  /*  @Test
-    public void testExisteJugador() {
-        System.out.println("Prueba de existeJugador");
-        String nombre = "";
-        ModeloDatos instance = new ModeloDatos();
-        boolean expResult = false;
-        boolean result = instance.existeJugador(nombre);
-        assertEquals(expResult, result);
-       // fail("Fallo forzado.");
-    }*/
 
     @Test
-    public void test() {
-        assertTrue(true);
+    public void testExisteJugador() {
+        Source source = new Source(url, dbUser, dbPass);
+        Table jugadores = new Table(source, "jugadores");  
+        assertThat(jugadores).column("nombre").hasValues("Carroll");     
     }
 
-   /* @Test
+    @Test
     public void testActualizarJugador() {
-        System.out.println("Prueba de actualizar jugador");       
-        ModeloDatos instance = new ModeloDatos(); 
-        instance.abrirConexion();
-        boolean result = instance.actualizarJugador();
-        boolean expResult = true;
-        assertEquals(expResult, result);
-       // fail("Fallo forzado.");
-    } */
+        ModeloDatos instance = new ModeloDatos();
+        instance.actualizarJugador("Carroll");
+        Source source = new Source(url, dbUser, dbPass);
+        Table jugadores = new Table(source, "Jugadores");        
+        assertThat(jugadores).row().hasValues("1","Carroll","1");
+   
+    }
 }
