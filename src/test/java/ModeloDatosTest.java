ed403b276f7921e35f7c884da66927a86b27474a
Luis Alberto Cobo <luisalberto.cobo@vass.es>
2020-12-24 11:35:27 +0100

    PU

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -5,20 +5,20 @@ import org.assertj.db.type.Table;
 
 public class ModeloDatosTest {
      // Con variables de entorno
-     String dbHost = System.getenv().get("DATABASE_HOST");
+    /* String dbHost = System.getenv().get("DATABASE_HOST");
      String dbPort = System.getenv().get("DATABASE_PORT");
      String dbName = System.getenv().get("DATABASE_NAME");
      String dbUser = System.getenv().get("DATABASE_USER");
      String dbPass = System.getenv().get("DATABASE_PASS");
 
      String url = dbHost + ":" + dbPort + "/" + dbName;
-
-    /*String dbHost = "jdbc:mysql://localhost";
+*/
+    String dbHost = "jdbc:mysql://localhost";
     String dbPort = "3306";
     String dbName = "baloncesto";
     String dbUser = "usuario";
     String dbPass = "clave";
-    String url = dbHost + ":" + dbPort + "/" + dbName;*/
+    String url = dbHost + ":" + dbPort + "/" + dbName;
 
 
     @Test
