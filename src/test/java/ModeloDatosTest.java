c8e8f6d6c5dbeeef8b098e1daa30d308759c4f84
Luis Alberto Cobo <luisalberto.cobo@vass.es>
2020-12-26 14:21:59 +0100

    Prueba unitaria

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -1,15 +1,44 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.assertj.db.type.Source;
+import org.assertj.db.type.Table;
 
 public class ModeloDatosTest {
+    public static  ModeloDatos instance;
+
+     // Con variables de entorno
+     String dbHost = System.getenv().get("DATABASE_HOST");
+     String dbPort = System.getenv().get("DATABASE_PORT");
+     String dbName = System.getenv().get("DATABASE_NAME");
+     String dbUser = System.getenv().get("DATABASE_USER");
+     String dbPass = System.getenv().get("DATABASE_PASS");
+
+     String url = dbHost + ":" + dbPort + "/" + dbName;
+
+     
+     @Test
+     public void testExisteJugador() {
+         System.out.println("Prueba de existeJugador");
+         String nombre = "";
+         ModeloDatos instance = new ModeloDatos();
+         boolean expResult = false;
+         boolean result = instance.existeJugador(nombre);
+         assertEquals(expResult, result);
+        // fail("Fallo forzado.");
+     }    
+
+
     @Test
-    public void testExisteJugador() {
-        System.out.println("Prueba de existeJugador");
-        String nombre = "";
-        ModeloDatos instance = new ModeloDatos();
-        boolean expResult = false;
-        boolean result = instance.existeJugador(nombre);
-        assertEquals(expResult, result);
-       // fail("Fallo forzado.");
+    public void testActualizarJugador() {   
+        instance = new ModeloDatos();
+        instance.abrirConexion();     
+        instance.actualizarJugador("Carroll");
+        Source source = new Source(url, dbUser, dbPass);
+        Table jugadores = new Table(source, "Jugadores");            
+        assertThat(jugadores).row().hasValues("1","Carroll","1");
+        instance.cerrarConexion();
+   
     }
 }
